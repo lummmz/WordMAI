c11ac6b95e1ff06c456867d4c8472a00737e6518
lummmz <68224337+lummmz@users.noreply.github.com>
2020-07-17 20:13:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,15 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile file (":/stylesheets.css");
+    file.open(QFile::ReadOnly);
+    a.setStyleSheet(file.readAll());
+
+
     MainWindow *mainWindow = new MainWindow();
     mainWindow->showMaximized();
     return a.exec();
+
 }
+
